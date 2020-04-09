73a4c1cab75b8f9fec3431dfd351e56d93cd9dab
jychan26 <jiayi5638@gmail.com>
2020-04-09 18:39:32 -0400

    made minor change

--- piece.h
+++ piece.h
@@ -13,13 +13,13 @@ class Piece { // abstract class
 	int force;
 
 	public:
-		char getPiece(); // get name field
-		Colour getPieceColour(); // get pieceColour fielid
-		int getForce(); // get force field
+	char getPiece(); // get name field
+	Colour getPieceColour(); // get pieceColour fielid
+	int getForce(); // get force field
 
-		virtual bool isObeyRule(Move) = 0; // does Move agree with Piece's movement (in terms of direction and distance)?
-		virtual vector<Position> getInBetweenPositions(Move); // get positions between Move that Piece could otherwise have validly landed on
-		virtual vector<Move> getLegalMoves(Position) = 0;
+	virtual bool isObeyRule(Move) = 0; // does Move agree with Piece's movement (in terms of direction and distance)?
+	virtual vector<Position> getInBetweenPositions(Move); // get positions between Move that Piece could otherwise have validly landed on
+	virtual vector<Move> getLegalMoves(Position) = 0;
 };
 
 #endif
