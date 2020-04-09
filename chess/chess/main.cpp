abaf5f070071e66248d4aa51517fc10cbd53444b
DaveHJT <49217609+DaveHJT@users.noreply.github.com>
2020-04-08 23:06:39 -0400

    hjt_test
    created new project with main.cc

---
+++ chess/chess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chess
+//
+//  Created by Jingtong Hu on 2020-04-08.
+//  Copyright © 2020 CS246Team. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
