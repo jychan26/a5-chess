74818d91a8590b194d6521e0c35966be3f6146cf
jychan26 <jiayi5638@gmail.com>
2020-04-18 23:22:15 -0400

    modified for two square move and diagonal capture

--- pawn.cc
+++ pawn.cc
@@ -16,19 +16,28 @@ void Pawn::setNofMove(int n) {
 }
 
 bool Pawn::isObeyRule(Move &m) const {
-	Position curr_pos{m.from};
-	Position next_pos{m.to};
-
-	int row_diff{next_pos.row - curr_pos.row};
-	int col_diff{next_pos.col - curr_pos.col};
-
 	// assumes:
 	// Move consists of valid initial and final positions
 	// initial and final positions are not equal
-	return ((col_diff == 0) and // vertical
-		((this->getFirstMove() and (row_diff <= 2)) or
-		  (not(this->getFirstMove()) and (row_diff == 1)))
-		);
+	Position curr_pos{m.from};
+	Position next_pos{m.to};
+
+	// row_diff is based on pieceColour to determine its "forward" motion
+	int row_diff = ((getPieceColour() == Colour::White) ? 
+			(next_pos.row - curr_pos.row) : (curr_pos.row - next_pos.row));
+	int col_diff = (next_pos.col - curr_pos.col);
+
+	// Booleans for regular move
+	bool is_forward_motion = ((col_diff == 0) and (row_diff > 0));
+	bool is_valid_first_distance = (this->getFirstMove() and (row_diff == 2));
+	bool is_valid_reg_distance = row_diff == 1;
+	bool is_valid_reg_move = (is_forward_motion and 
+				 (is_valid_first_distance or is_valid_reg_distance));
+	
+	// Booleans for capturing pieces
+	bool is_valid_capture_move = ((row_diff == 1) and (abs(col_diff) == 1));
+
+	return (is_valid_reg_move or is_valid_capture_move);
 }
 
 vector<Move*> Pawn::getLegalMoves(Position pos) const {
@@ -44,4 +53,23 @@ vector<Move*> Pawn::getLegalMoves(Position pos) const {
     dest.col -= 2;
     if (dest.isValid()) moves.push_back(new Move(pos, dest));
 	return moves;
+}
+
+vector<Position> Pawn::getInBetweenPositions(Move &m) const {
+	vector<Position> positions;
+	Position curr_pos{m.from};
+	Position next_pos{m.to};
+
+	// assumes Move is valid 
+	int row_diff = abs(next_pos.row - curr_pos.row);
+	if (this->getFirstMove() and (row_diff == 2)) {
+		if (getPieceColour() == Colour::White) {
+			Position mid_pos{curr_pos.row + 1, curr_pos.col};
+			positions.emplace_back(mid_pos);
+		} else {
+			Position mid_pos{curr_pos.row - 1, curr_pos.col};
+			positions.emplace_back(mid_pos);
+		}
+	}
+	return positions;
 }
